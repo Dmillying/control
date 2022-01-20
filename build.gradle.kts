b6baf0c2b46d337e3443a0a199cb94bf0dce89cd
IDmilly <dmilly@outlook.kr>
2022-01-20 14:50:48 +0900

    feat: control 1.18

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.6.10"
+}
+
+group = "com.github.idmilly.control"
+version = "1.0"
+
+repositories {
+    mavenCentral()
+    maven { url = uri("https://papermc.io/repo/repository/maven-public/") }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    compileOnly("io.papermc.paper:paper-api:1.18.1-R0.1-SNAPSHOT")
+}
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
---
+++ src/main/kotlin/com/github/idmilly/control/ControlPlugin.kt
@@ -0,0 +1,88 @@
+package com.github.idmilly.control
+
+import io.papermc.paper.event.player.AsyncChatEvent
+import net.md_5.bungee.api.ChatColor
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.block.BlockPlaceEvent
+import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.event.entity.EntityDamageEvent
+import org.bukkit.event.inventory.CraftItemEvent
+import org.bukkit.event.inventory.InventoryOpenEvent
+import org.bukkit.event.player.*
+import org.bukkit.plugin.java.JavaPlugin
+
+class ControlPlugin : JavaPlugin(), Listener {
+    override fun onEnable() {
+        logger.info("${ChatColor.GREEN}control enable.")
+        server.pluginManager.registerEvents(this, this)
+
+        saveDefaultConfig()
+    }
+
+    @EventHandler
+    private fun blockPlace(event: BlockPlaceEvent) {
+        if (config.getBoolean("event.blockPlace")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun blockBreak(event: BlockBreakEvent) {
+        if (config.getBoolean("event.blockBreak")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun playerInteract(event: PlayerInteractEvent) {
+        if (config.getBoolean("event.playerInteract")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun playerMove(event: PlayerMoveEvent) {
+        if (config.getBoolean("event.playerMove")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun playerPortal(event: PlayerPortalEvent) {
+        if (config.getBoolean("event.playerPortal")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun asyncChat(event: AsyncChatEvent) {
+        if (config.getBoolean("event.asyncChat")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun inventoryOpen(event: InventoryOpenEvent) {
+        if (config.getBoolean("event.inventoryOpen")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun playerJoinMessage(event: PlayerJoinEvent) {
+        if (config.getBoolean("event.playerJoinMessage")) { event.joinMessage(null) }
+    }
+
+    @EventHandler
+    private fun playerQuitMessage(event: PlayerQuitEvent) {
+        if (config.getBoolean("event.playerQuitMessage")) { event.quitMessage(null) }
+    }
+
+    @EventHandler
+    private fun craftItem(event: CraftItemEvent) {
+        if (config.getBoolean("event.craftItem")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun entityDamage(event: EntityDamageEvent) {
+        if (config.getBoolean("event.entityDamage")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun entityDamageByEntity(event: EntityDamageByEntityEvent) {
+        if (config.getBoolean("event.entityDamageByEntity")) { event.isCancelled = true }
+    }
+
+    @EventHandler
+    private fun playerCommandPreprocess(event: PlayerCommandPreprocessEvent) {
+        if (config.getBoolean("event.playerCommandPreprocess")) { event.isCancelled = true }
+    }
+}

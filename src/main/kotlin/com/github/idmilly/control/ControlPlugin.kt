c60c9428ccdabf728fbeea3dbf214eb647663437
idmilly <dmilly@outlook.kr>
2022-01-30 01:24:24 +0900

    ✨ permission

--- src/main/kotlin/com/github/idmilly/control/ControlPlugin.kt
+++ src/main/kotlin/com/github/idmilly/control/ControlPlugin.kt
@@ -21,49 +21,48 @@ class ControlPlugin : JavaPlugin(), Listener {
         saveDefaultConfig()
     }
 
-    @EventHandler
     private fun blockPlace(event: BlockPlaceEvent) {
-        if (config.getBoolean("event.blockPlace")) { event.isCancelled = true }
+        if (config.getBoolean("event.blockPlace") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun blockBreak(event: BlockBreakEvent) {
-        if (config.getBoolean("event.blockBreak")) { event.isCancelled = true }
+        if (config.getBoolean("event.blockBreak") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun playerInteract(event: PlayerInteractEvent) {
-        if (config.getBoolean("event.playerInteract")) { event.isCancelled = true }
+        if (config.getBoolean("event.playerInteract") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun playerMove(event: PlayerMoveEvent) {
-        if (config.getBoolean("event.playerMove")) { event.isCancelled = true }
+        if (config.getBoolean("event.playerMove") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun playerPortal(event: PlayerPortalEvent) {
-        if (config.getBoolean("event.playerPortal")) { event.isCancelled = true }
+        if (config.getBoolean("event.playerPortal") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun asyncChat(event: AsyncChatEvent) {
-        if (config.getBoolean("event.asyncChat")) { event.isCancelled = true }
+        if (config.getBoolean("event.asyncChat") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun inventoryOpen(event: InventoryOpenEvent) {
-        if (config.getBoolean("event.inventoryOpen")) { event.isCancelled = true }
+        if (config.getBoolean("event.inventoryOpen") && !event.player.isOp) { event.isCancelled = true }
     }
 
     @EventHandler
     private fun playerJoinMessage(event: PlayerJoinEvent) {
-        if (config.getBoolean("event.playerJoinMessage")) { event.joinMessage(null) }
+        if (config.getBoolean("event.playerJoinMessage") && !event.player.isOp) { event.joinMessage(null) }
     }
 
     @EventHandler
     private fun playerQuitMessage(event: PlayerQuitEvent) {
-        if (config.getBoolean("event.playerQuitMessage")) { event.quitMessage(null) }
+        if (config.getBoolean("event.playerQuitMessage") && !event.player.isOp) { event.quitMessage(null) }
     }
 
     @EventHandler
@@ -83,6 +82,6 @@ class ControlPlugin : JavaPlugin(), Listener {
 
     @EventHandler
     private fun playerCommandPreprocess(event: PlayerCommandPreprocessEvent) {
-        if (config.getBoolean("event.playerCommandPreprocess")) { event.isCancelled = true }
+        if (config.getBoolean("event.playerCommandPreprocess") && !event.player.isOp) { event.isCancelled = true }
     }
 }
